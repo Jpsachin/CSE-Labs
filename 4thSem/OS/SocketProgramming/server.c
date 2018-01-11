b75c0d622af1ed79c2e62be80444c2bf8862580c
Omkar Srinivas Prabhu <omkkarprabuinfinity@gmail.com>
2018-01-11 11:28:16 +0000

    4th sem os lab update server

--- 4thSem/OS/SocketProgramming/server.c
+++ 4thSem/OS/SocketProgramming/server.c
@@ -2,6 +2,7 @@
 
 // Reference
 // http://www.csd.uoc.gr/~hy556/material/tutorials/cs556-3rd-tutorial.pdf
+// https://www.ibm.com/support/knowledgecenter/en/SSLTBW_2.2.0/com.ibm.zos.v2r2.halc001/socket_parm.htm
 // https://stackoverflow.com/questions/16508685/understanding-inaddr-any-for-socket-programming
 
 #include <stdio.h>
@@ -14,7 +15,7 @@
 // header file for constants and structures needed for internet domain addresses
 #include <netinet/in.h>
 
-#define INTERNET_PROTOCOL 0
+#define DEFAULT_PROTOCOL 0
 
 int socketDescriptorServer;
 int socketAfterAcceptDescriptor;
@@ -25,8 +26,8 @@ struct sockaddr_in serverAddr;
 int main(){
 
 
-	// create socket for communication for the server with IPv4, TCP, IP types of protocols
-	if ((socketDescriptorServer = socket(AF_INET, SOCK_STREAM, INTERNET_PROTOCOL)) == -1) {
+	// create socket for communication for the server with IPv4, TCP type of socket, TCP protocol
+	if ((socketDescriptorServer = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP)) == -1) {
         printf("\n\n=>=> Socket creation error \n");
         return -1;
     }
@@ -35,6 +36,7 @@ int main(){
     serverAddr.sin_family = AF_INET;
     // use a random port preferably not in use
     // htons(): converts host byte order to network byte order.
+    //  big-endian byte order is also referred to as network byte order.
     serverAddr.sin_port = htons(5100);
     // specify server address 0.0.0.0
     // used to bind socket to any ip address alloted to the system
@@ -62,17 +64,17 @@ int main(){
 
             // get client
             // accept first socket in the queue of pending connections passing socketDescriptorServer, place to store information about client (leaving NULL for convenience)
-            // returns: client socket no
+            // returns: new socket file descriptor, the socket file also contains information about client socket struct 
             //          -1 error
-            int clientSocket = accept(socketDescriptorServer, NULL, NULL);
+            int newSocket = accept(socketDescriptorServer, NULL, NULL);
 
             // respond to request
             while(done){
 
-                if(clientSocket != -1){
+                if(newSocket != -1){
 
                     // wait for request passing clientSocket, buffer to store msg into, size, special flag (usually 0)
-            		int count = recv(clientSocket, resBuffer, sizeof(resBuffer), 0);
+            		int count = recv(newSocket, resBuffer, sizeof(resBuffer), 0);
 
             		if(count > 0){
             		    printf("\n\n=>=> Response from Client: %s", resBuffer);
@@ -83,7 +85,7 @@ int main(){
                     	gets(msg);
 
                     	// send response to client passing clientSocket, message, size, special flag (usually 0)
-                    	if (send(clientSocket, msg, sizeof(msg), 0) == -1){
+                    	if (send(newSocket, msg, sizeof(msg), 0) == -1){
                     		printf("\n\n=>=> Message not sent \n");
                     	}
 
@@ -94,7 +96,7 @@ int main(){
             }
 
             // closed connection with this client
-            close(clientSocket);
+            close(newSocket);
 
         }
 
