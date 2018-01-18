67bd335eecc8b86c54c2b9be247ebd4129f83678
Omkar Srinivas Prabhu <omkkarprabuinfinity@gmail.com>
2018-01-18 11:29:27 +0000

    Create roundRobin.c

---
+++ 4thSem/OS/ProcessScheduling/roundRobin.c
@@ -0,0 +1,119 @@
+#include<stdio.h>
+#define NO_OF_PROCESSES 5
+#define TIME_QUANTUM 2
+
+// result variables 
+int waitingTime[NO_OF_PROCESSES] = {0};
+int turnAroundTime[NO_OF_PROCESSES] = {0};
+int completed[NO_OF_PROCESSES] = {0};
+int totalTurnAroundTime = 0;
+int totalWaitingTime = 0;
+
+// function declarations
+void roundRobin(int, int [], int, int []);
+
+
+
+int main(){
+
+    // process variables
+    int n = NO_OF_PROCESSES;
+    int burstTime[NO_OF_PROCESSES] = {3, 1, 6, 2, 8};
+    int c_burstTime[NO_OF_PROCESSES] = {3, 1, 6, 2, 8};
+   
+    int timeQuantum = TIME_QUANTUM;
+    
+    // implement the algorithm
+    roundRobin(n, burstTime, timeQuantum, c_burstTime); 
+    
+    
+    // display results
+    printf("Process ID\t\tBurst Time\t\tWaiting Time\t\tTurn Around Time\n");
+     
+    int i;
+    for (i = 0; i < n; i++){
+        
+        printf("%d\t\t\t%d\t\t\t%d\t\t\t%d\n\n", i, c_burstTime[i], waitingTime[i], turnAroundTime[i]); 
+        
+        totalWaitingTime += waitingTime[i];
+        totalTurnAroundTime += turnAroundTime[i]; 
+    }   
+
+    printf("Averge Wait Time: %.3f\nAverage Turnaround Time: %.3f\n", (float)totalWaitingTime/n, (float)totalTurnAroundTime/n);   
+
+    return 0;
+}
+
+
+
+/**
+ *  Round Robin Scheduling Algorithm
+ */
+void roundRobin(int n, int b[], int t, int c_b[]){
+    
+    
+    int i, j, k;
+    
+    // completion status for all process 
+    int allProcessesCompleted = n;
+   
+   
+    // calculate waiting time for all processes
+    // repeat until all process are completed
+    while (allProcessesCompleted){
+        
+        // execute all process
+        for (i = 0; i < n; i++){
+            
+            // check if a process has completed status
+            if (completed[i] == 0){
+                
+                
+                // check if time quantum is greater than the burst time of the process to which token is allocated         
+                if(b[i] - t <= 0){
+                    
+
+                    for (k = 0; k < n; k++){
+                    
+                        if (k != i && completed[k] == 0){
+                            waitingTime[k] += b[i];
+                        }
+                    }
+                    
+                    completed[i] = 1;
+                    allProcessesCompleted--;
+                                    
+                    b[i] = 0;
+                }
+                else{
+                    
+                    for (k = 0; k < n; k++){
+                    
+                        if (k != i && completed[k] == 0){
+                            waitingTime[k] += t;    
+                        }
+                    }
+                    
+                    // 
+                    b[i] = b[i] - t; 
+                    
+                }
+            
+            }
+     
+        }
+        
+    }
+    
+    // calculate turn around time for each process
+    for (i = 0; i < n; i++){
+        
+        turnAroundTime[i] = waitingTime[i] + c_b[i];
+        
+    }   
+    
+} 
+
+
+
+
